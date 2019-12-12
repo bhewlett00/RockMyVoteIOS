38c2982fd86a0b8371bedcc2b8314fc14867796b
Bri Hewlett <bhewlett00@gmail.com>
2019-12-12 09:57:39 -0500

    Implemented logic fro Rock My Vote App

--- Rock the Vote/ViewController.swift
+++ Rock the Vote/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
 
+    var votes = [Vote] ()
+
+    @IBOutlet weak var songTextField: UITextField!
+    
+    @IBOutlet weak var voterTextField: UITextField!
+    
+    @IBOutlet weak var like: UIButton!
+        
+    @IBOutlet weak var dislike: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func saveVotes(){
+        do{
+            try context.save()
+        }catch{
+            print("Error saving Vote to Core Data")
+        }
+        
+        reloadData()
+    }
 
+    @IBAction func like(_ sender: UIButton) {
+        let newVote = Vote(context: self.context)
+            
+        newVote.songTitle = self.songTextField.text!
+        newVote.voterName = self.voterTextField.text!
+        newVote.vote = true
+            
+        self.votes.append(newVote)
+            
+        self.saveVotes()
+    }
+    
+    
+    @IBAction func dislike(_ sender: UIButton) {
+        let newVote = Vote(context: self.context)
+            
+        newVote.songTitle = self.songTextField.text!
+        newVote.voterName = self.voterTextField.text!
+        newVote.vote = false
+            
+        self.votes.append(newVote)
+            
+        self.saveVotes()
+    }
+    
 
 }
 
