fd07be457bdbcc2eb7787e37355c179d51b2fb37
Bri Hewlett <bhewlett00@gmail.com>
2019-12-12 08:02:16 -0500

    Initial Commit

---
+++ Rock the Vote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rock the Vote
+//
+//  Created by Hewlett, Brianna Anne on 12/12/19.
+//  Copyright © 2019 Hewlett, Brianna Anne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
